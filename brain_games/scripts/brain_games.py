0020857b17fc5b2b9c2551b481a740e3fbef9fea
Konstantin Mayev <k.mayev@gmail.com>
2020-05-17 20:18:09 +0300

    v.0.2.0
    cli.py with welcome_user() added
    Makefile added
    brain_games.py added import from cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-
 """An example script."""
+from brain_games import cli
 
 def main():
-"""Run an example code."""
-	print('Welcome to the Brain Games!')    
+	"""Run an example code."""
+	print('Welcome to the Brain Games!') 
+	cli.welcome_user()   
     
 if __name__ == '__main__':
     main()
