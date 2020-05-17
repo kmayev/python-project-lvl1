5dc7705b3ece0a4061e66f2aa2c2a6638d908c55
Konstantin Mayev <k.mayev@gmail.com>
2020-05-17 16:45:22 +0300

    First commit of brain_games

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""An example script."""
+
+def main():
+"""Run an example code."""
+	print('Welcome to the Brain Games!')    
+    
+if __name__ == '__main__':
+    main()
